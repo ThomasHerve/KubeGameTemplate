fda31a50b2eb706a7461665c194cace724eda21a
Thomas <therve38@gmail.com>
2026-08-18 11:19:00 +0200

    Working on spawing instances

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i "s|BACKEND_URL_TEMPLATE|${BACKEND_URL}|g" /files/index.html
-sed -i "s|BACKEND_PROTOCOL_TEMPLATE|${BACKEND_PROTOCOL}|g" /files/index.html
+sed -i "s|BACKEND_URL_TEMPLATE|${BACKEND_URL}|g" /var/cache/nginx/index.html
+sed -i "s|BACKEND_PROTOCOL_TEMPLATE|${BACKEND_PROTOCOL}|g" /var/cache/nginx/index.html
 
 exec nginx -g "daemon off;"
