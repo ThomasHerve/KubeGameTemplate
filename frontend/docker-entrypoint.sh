06dd599cf9a96d94210a689213101f9e7368320f
Thomas <therve38@gmail.com>
2026-08-18 11:06:38 +0200

    Working on spawing instances

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-envsubst '${BACKEND_URL} ${BACKEND_PROTOCOL}' \
-    < /usr/share/nginx/html/index.html \
-    > /usr/share/nginx/html/index.html
-
+sed -i "s|BACKEND_URL_TEMPLATE|${BACKEND_URL}|g" /usr/share/nginx/html/index.html
+sed -i "s|BACKEND_PROTOCOL_TEMPLATE|${BACKEND_PROTOCOL}|g" /usr/share/nginx/html/index.html
 
 exec nginx -g "daemon off;"
